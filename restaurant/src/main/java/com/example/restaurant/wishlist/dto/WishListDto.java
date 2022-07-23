31ad87d03d457c5d7b07fef43e0f12d7ccff1a17
bangjaeyoung <qkdwo4613@gmail.com>
2022-07-23 23:34:39 +0900

    first commit

--- restaurant/src/main/java/com/example/restaurant/wishlist/dto/WishListDto.java
+++ restaurant/src/main/java/com/example/restaurant/wishlist/dto/WishListDto.java
@@ -1,7 +1,6 @@
-package com.example.restaurant.wishlist.entity;
+package com.example.restaurant.wishlist.dto;
 
 
-import com.example.restaurant.db.MemoryDbEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +10,9 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class WishListEntity extends MemoryDbEntity {
+public class WishListDto{
+
+    private Integer index;
 
     private String title;                   // 음식명, 장소명
     private String category;                // 카테고리
@@ -22,4 +23,5 @@ public class WishListEntity extends MemoryDbEntity {
     private boolean isVisit;                // 방문 여부
     private int visitCount;                 // 방문 카운트
     private LocalDateTime lastVisitDate;    // 마지막 방문 일자
+
 }
